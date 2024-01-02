3ec0ee1de6aa38208e3652dd0162f00200507f02
ilikezw4 <92365142+ilikezw4@users.noreply.github.com>
2024-01-02 20:36:58 +0100

    added map generation (working on normal map )

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {MainComponent} from "./components/main/main.component";
 
 const routes: Routes = [
   {
@@ -10,6 +11,10 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'main',
+    component: MainComponent,
   },
 ];
 
